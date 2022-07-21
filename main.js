4ad02fcda742edf5b3f74ba914d38b4980101c39
salman-dropbox <109694394+salman-dropbox@users.noreply.github.com>
2022-07-21 11:17:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  
+  renderLeads()
+})
+console.log(localStorage.getItem("myLeads"))
+
+
+function renderLeads() {
+let listItems = ""
+
+for (let i = 0; i < myLeads.length; i++) {
+  //listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>" + " "
+  
+  listItems += 
+  `<li>
+  <a href="${myLeads[i]}" taget="_blank"> ${myLeads[i]} 
+  </a>
+  </li>`
+}
+
+ulEl.innerHTML = listItems
+}
+
+
+
